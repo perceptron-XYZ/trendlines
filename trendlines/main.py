30d220f8728daad95ad322eb0a0506f92228925f
Ryan Xingyu Zhou <ryan.zxy@gmail.com>
2015-12-25 01:33:43 -0500

    add weather lookup support

--- trendlines/main.py
+++ trendlines/main.py
@@ -2,13 +2,15 @@
 """ trendlines command line tool
 
 Usage:
-    trendlines [-n NUMBER] [-s CRITERIA] [-d DATE] KEYWORD
+    trendlines news KEYWORD [-n NUMBER] [-s CRITERIA] [-d DATE]
+    trendlines weather CITY [-m METRICS]
 
 Options:
     -h --help             show this screen
     -n --number=NUMBER    specify the number of results displayed
     -s --sortby=CRITERIA  specify the ranking criteria
     -d --date=DATE        specify the search date
+    -m --metrics=METRICS  specify the unit for weather display
 
 """
 import docopt
@@ -16,11 +18,12 @@ import requests
 from bcolors import BColors
 
 wordlink_api = 'http://wordlink.com/search.php?'
+weather_api = 'http://api.openweathermap.org/data/2.5/weather?'
 
 def parse_arguments():
     return docopt.docopt(__doc__)
 
-def formulate_request(url,arguments):
+def formulate_request_news(url,arguments):
     number = arguments['--number'] if arguments['--number'] else ''
     sortby = arguments['--sortby'] if arguments['--sortby'] else ''
     date = arguments['--date'] if arguments['--date'] else ''
@@ -29,23 +32,37 @@ def formulate_request(url,arguments):
             +'&sortby='+sortby
     return request
 
+def formulate_request_weather(url,city,metrics,api):
+    request = url + 'q='+ city + '&units='+metrics+'&appid='+api
+    return request
+
 def main():
     arguments = parse_arguments()
-    request = formulate_request(wordlink_api,arguments)
-    response = requests.get(request)
-    num_hits = response.json()['hits']['total']
-    hits = response.json()['hits']['hits']
+    if arguments['news']:
+        request = formulate_request_news(wordlink_api,arguments)
+        response = requests.get(request)
+        num_hits = response.json()['hits']['total']
+        hits = response.json()['hits']['hits']
 
-    num_display = int(arguments['--number'])
-    for count,hit in enumerate(hits):
-        if (count+1) > num_display:
-            break
-        else:
-            title = hit['_source']['doc']['title'].encode('utf-8')
-            url = hit['_source']['doc']['url'].encode('utf-8')
-            print ('{count}     {title}').format(count=count+1,title=title)
-            print BColors.OKBLUE+ '      {url}'.format(url=url) \
-                  + BColors.ENDC
+        num_display = int(arguments['--number']) if arguments['--number'] else 10
+        for count,hit in enumerate(hits):
+            if (count+1) > num_display:
+                break
+            else:
+                title = hit['_source']['doc']['title'].encode('utf-8')
+                url = hit['_source']['doc']['url'].encode('utf-8')
+                print ('{count}     {title}').format(count=count+1,title=title)
+                print BColors.OKBLUE+ '      {url}'.format(url=url) \
+                      + BColors.ENDC
+
+    elif arguments['weather']:
+        api_id = '2de143494c0b295cca9337e1e96b00e0'
+        city = arguments['CITY']
+        metrics = arguments['--metrics'] if arguments['--metrics'] in ['metric','imperial'] else 'metric'
+        request = formulate_request_weather(weather_api,city,metrics,api_id)
+        response = requests.get(request).json()
+        degree = response['main']['temp']
+        print 'weather service in development', 'temp:', degree
 
 
 
